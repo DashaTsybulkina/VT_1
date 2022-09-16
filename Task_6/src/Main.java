f86580b957e03318b7c86f56b00d16036515f68e
DashaTsybulkina <cybulkinadara@gmail.com>
2022-09-16 19:02:57 +0300

    task6

---
+++ Task_6/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+
+public class Main {
+    final static int N = 5;
+
+    public static void main(String[] args) {
+        int[] numbers = new int[N];
+        Random rand = new Random();
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = rand.nextInt(50);
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println();
+        int[][] matrix =fillMatrix(numbers);
+        printMatrix(matrix);
+    }
+
+    public static int[][] fillMatrix(int[] numbers){
+        int[][] matrix = new int[numbers.length][numbers.length];
+        int temp = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                temp = (i + j) % matrix.length;
+                matrix[i][j] = numbers[temp];
+            }
+        }
+        return matrix;
+    }
+
+    public static void printMatrix(int[][] matrix){
+        System.out.println("Последовательность:");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] +" ");
+            }
+            System.out.println();
+        }
+    }
+}
